8373d4a516c5e5db8a32b9d50bcc1dbcd3294a91
zettaittenani <zettaittenani@gmail.com>
2018-03-08 20:43:21 +0900

    Impl P07

--- answers/P07.scala
+++ answers/P07.scala
@@ -1,5 +1,9 @@
 object P07 {
-  def flatten(list: List[Any]): List[Any] = {
-    list.flatten // return Type: scala.collection.GenTraversableOnce[B]
+  def flatten[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case head :: tail => (head match {
+      case l: List[T] => flatten(l)
+      case i => List(i)
+    }) ::: flatten(tail)
   }
 }
