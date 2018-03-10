91adaeb7803bcff88977b6ef7834a725f1c06676
zettaittenani <zettaittenani@gmail.com>
2018-03-10 16:26:48 +0900

    Use anonymous fn at P16

--- answers/P11-P20/P16.scala
+++ answers/P11-P20/P16.scala
@@ -1,3 +1,3 @@
 object P16 {
-  def drop[T](n: Int, list: List[T]): List[T] = list.zipWithIndex.filter(e => (e._2 + 1) % n != 0).map(x => x._1)
+  def drop[T](n: Int, list: List[T]): List[T] = list.zipWithIndex.filter(e => (e._2 + 1) % n != 0).map(_._1)
 }
