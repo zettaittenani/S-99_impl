f3b4f9dc8255c21a6f555459955e106bf9e9a5ac
zettaittenani <zettaittenani@gmail.com>
2018-03-10 14:59:32 +0900

    Change directory configuration

---
+++ answers/P11-P20/P11.scala
@@ -0,0 +1,14 @@
+object P11 {
+  // TODO
+
+  def encode(list: List[Any]): List[(Int, Any)] = pack(list).map(x => (x.length, x.head))
+
+  def pack(list: List[Any]): List[List[Any]] = {
+    if (list.isEmpty) List(List())
+    else {
+      val (packed, next) = list.span{_ == list.head}
+      if (next.isEmpty) List(packed)
+      else packed :: pack(next)
+    }
+  }
+}
