8210c304c7caaee2463dde3635a5d969533ff191
zettaittenani <zettaittenani@gmail.com>
2018-03-10 14:49:12 +0900

    Impl P10 and brush up P09

--- answers/P10.scala
+++ answers/P10.scala
@@ -1,4 +1,6 @@
-object P09 {
+object P10 {
+  def encode(list: List[Any]): List[(Int, Any)] = pack(list).map(x => (x.length, x.head))
+
   def pack(list: List[Any]): List[List[Any]] = {
     if (list.isEmpty) List(List())
     else {
