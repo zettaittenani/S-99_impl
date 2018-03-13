f872073e1b5188e9916d22afb26bc3c6222c3a7e
zettaittenani <zettaittenani@gmail.com>
2018-03-13 21:44:26 +0900

    Refactor P23

--- answers/P21-P30/P23.scala
+++ answers/P21-P30/P23.scala
@@ -1,5 +1,6 @@
 object P23 {
   import scala.util.Random
+
   def randomSelect[T](n: Int, list: List[T]): List[T] = n match {
     case _ if (n < 0 || list.length < n) => throw new NoSuchElementException
     case _ => Random.shuffle(list).take(n)
