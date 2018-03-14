e336b3561b52152f90ed8e3650cd4d8340a3fd98
zettaittenani <zettaittenani@gmail.com>
2018-03-14 21:40:10 +0900

    Impl P26

---
+++ answers/P21-P30/P26.scala
@@ -0,0 +1,13 @@
+object P26 {
+  def flatMapSublists[A, B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
+    ls match {
+      case Nil => Nil
+      case sublist @ (_ :: tail) => f(sublist) ::: flatMapSublists(tail)(f)
+    }
+
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else flatMapSublists(ls) { sl =>
+      combinations(n - 1, sl.tail) map { sl.head :: _ }
+    }
+}
